660eab7f23d667412045789ed3216adf8079af52
Giannis Chatziveroglou <gchatz@mit.edu>
2022-08-16 18:00:56 -0400

    bump version

--- src/idl/cardinal_namespaces.ts
+++ src/idl/cardinal_namespaces.ts
@@ -1,5 +1,5 @@
 export type Namespaces = {
-  version: "4.1.49";
+  version: "4.1.50";
   name: "namespaces";
   instructions: [
     {
@@ -2009,7 +2009,7 @@ export type Namespaces = {
 };
 
 export const IDL: Namespaces = {
-  version: "4.1.49",
+  version: "4.1.50",
   name: "namespaces",
   instructions: [
     {
