44d7ad9eff775a6ca3c4b94b5160dfdf948db5fb
jpbogle <jpbogle22@gmail.com>
2022-08-31 23:38:03 -0700

    Change region

--- api/events/common.ts
+++ api/events/common.ts
@@ -43,7 +43,7 @@ export const sendEmail = async (
 ) => {
   const ses = new SES({
     apiVersion: "2010-12-01",
-    region: "us-east-1",
+    region: "us-west-2",
     accessKeyId: process.env.SES_ACCESS_KEY_ID,
     secretAccessKey: process.env.SES_SECRET_ACCESS_KEY,
   });
