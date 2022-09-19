28103cb81fbc9090809d249b1e00750daabd87b1
Avinash Jain <avinashkalpitjain@gmail.com>
2022-09-18 22:07:15 -0700

    Updated email ux

--- api/events/email.ts
+++ api/events/email.ts
@@ -11,11 +11,13 @@ export const approvalSuccessfulEmail = (
   claimURLs: string[],
   config: string | null
 ) => `
-<div>
+<div style="display:flex; justify-content: center;">
+<div style="width:786px; background-color: #f8f8f8; border-radius: 20px; font-family:sans-serif;">
 <img src=${getEventBannerImage(
   event.docId
 )} alt="event-image" style="width: 100%; max-width: 1000px; margin: auto; border-radius: 3%;">
 <h3>
+<div style=" padding: 20px;  ">
 Thank you for purchasing ${claimURLs.length} ${ticketName} ${
   claimURLs.length === 1 ? "ticket" : "tickets"
 } to <a href="${eventUrl(event.shortLink, config)}">${event.eventName}</a>!
@@ -29,9 +31,9 @@ Use the ${claimURLs.length === 1 ? "link" : "links"} below to claim your NFT ${
 ${claimURLs
   .map(
     (url, index) =>
-      `Ticket ${
+      `<b>Ticket ${
         index + 1
-      }: Claim Ticket <a href=${url}>From Laptop</a> or <a href=${`https://phantom.app/ul/browse/${encodeURIComponent(
+      }:</b> Claim Ticket <a href=${url}>From Laptop</a> or <a href=${`https://phantom.app/ul/browse/${encodeURIComponent(
         url
       )}`}>From Mobile</a> (Phantom Wallet Required)<br/></br/>`
   )
@@ -39,13 +41,34 @@ ${claimURLs
 <br />
 ${
   claimURLs.length === 1
-    ? "This is a ONE TIME USE only link. Feel free to share this link to anyone you want to claim your ticket."
-    : "These are ONE TIME USE only links. Feel free to share these links them with your friends or anyone that is coming with you."
+    ? "<i>Note: This is a ONE TIME USE only link. Feel free to share this link to anyone you want to claim your ticket.</i>"
+    : "<i>Note: These are ONE TIME USE only links. Feel free to share these links them with your friends or anyone that is coming with you.</i>"
 }
-<br/><br/>
+<br/><br/><hr/>
+<h3 style="margin-bottom: 0; padding-bottom: 0;">Event Details:</h3> <br/>  
+
+<b>📍 Location: </b><u>
+    <a
+      target='_blank'
+      rel="noreferrer" 
+      href=${`https://maps.google.com/?q=${event.eventLocation}`}
+    >
+    ${event.eventLocation}
+    </a></u> <br/>
+  <b>⏱ Date and Time: </b> ${new Date(event.eventStartTime).toLocaleTimeString(
+    [],
+    {
+      hour: "numeric",
+      minute: "2-digit",
+    }
+  )} <br/>
+  <b>🖊 Event Description:</b> <br/><br/>
+    ${event.eventDescription}
+
+    <br/><br/><hr/><br/>
 Best,<br/>
 <a href="https://www.cardinal.so/">The Cardinal Team</a>
-</div>`;
+</div></div>`;
 
 export const sendEmail = async (destination: string, data: string) => {
   const ses = new SES({
