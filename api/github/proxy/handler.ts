3a2a1d5c068321b2d8ede2019e618a6554ac7c88
Giannis Chatziveroglou <gchatz@mit.edu>
2022-10-04 11:45:36 -0700

    deploy dev (#191)

--- api/github/proxy/handler.ts
+++ api/github/proxy/handler.ts
@@ -7,7 +7,7 @@ module.exports.proxy = async (event) => {
   const responseHeaders = {
     "Access-Control-Allow-Methods": "*",
     "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-    "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
+    "Access-Control-Allow-Credentials": true, // Required for cookies and authorization headers with HTTPS
   };
 
   if (!params.username) {
